25b30e13ad12c915b84394e301d1c5b25fd0f72b
Dan6erbond <moravrav@gmail.com>
2021-01-24 15:28:09 +0100

    Setup TypeORM with SQLite3 config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
