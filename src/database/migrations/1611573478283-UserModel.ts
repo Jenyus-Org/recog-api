ab75f5642cd57e1fc4c282d6640f9b96f28c7f25
Dan6erbond <moravrav@gmail.com>
2021-01-25 12:25:46 +0100

    Create TypeORM user entity and migration

---
+++ src/database/migrations/1611573478283-UserModel.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class UserModel1611573478283 implements MigrationInterface {
+  name = "UserModel1611573478283";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "user",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "username",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "password",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "first_name",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "last_name",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("user");
+  }
+}
---
+++ src/users/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ name: "first_name" })
+  firstName: string;
+
+  @Column({ name: "last_name" })
+  lastName: string;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+}
