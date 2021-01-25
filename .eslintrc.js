a04e3cdaaaa1aa4522ecd3406ba4083ceb6991cf
Dan6erbond <moravrav@gmail.com>
2021-01-25 10:04:30 +0100

    Use Prettier config in ESLint instead of inline rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,13 +21,6 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: false,
-        trailingComma: "all",
-        endOfLine: "auto",
-      },
-    ],
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
   },
 };
