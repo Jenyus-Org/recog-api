95d1249082714573315ec5387372741413847c01
Dan6erbond <moravrav@gmail.com>
2021-01-26 19:09:56 +0100

    Create user controller with get and create endpoints

---
+++ src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { RegisterUserDto } from "./dto/registerUser.dto";
+import { UserDto } from "./dto/user.dto";
+import { UsersService } from "./users.service";
+
+@Controller("users")
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get(":username")
+  async findOne(@Param("username") username: string) {
+    const user = await this.usersService.findOne({ username });
+    return user && new UserDto(user);
+  }
+
+  @Post()
+  async register(@Body() registerInput: RegisterUserDto) {
+    const user = await this.usersService.create(registerInput);
+    return new UserDto(user);
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./user.entity";
+import { UsersController } from "./users.controller";
 import { UsersResolver } from "./users.resolver";
 import { UsersService } from "./users.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersResolver, UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
