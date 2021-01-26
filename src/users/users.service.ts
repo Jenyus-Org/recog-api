57c047ac3ad5ee554b0005c7b12f22c5e1e75f44
Dan6erbond <moravrav@gmail.com>
2021-01-26 19:08:24 +0100

    Create user module and service with find and create methods

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { RegisterUserInput } from "./input/registerUser.input";
+import { User } from "./user.entity";
+
+interface FindOneArgs {
+  id?: number;
+  username?: string;
+}
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findOne({ id, username }: FindOneArgs) {
+    if (id) {
+      return await this.usersRepository.findOne(id);
+    } else if (username) {
+      return await this.usersRepository.findOne({ username });
+    } else {
+      throw new Error("One of ID or username must be provided.");
+    }
+  }
+
+  async create(input: RegisterUserInput) {
+    const user = this.usersRepository.create(input);
+    const res = await this.usersRepository.save(user);
+    return res;
+  }
+}
