f565e0e3346753bfa657fa900d2786735ba1594a
Dan6erbond <moravrav@gmail.com>
2021-01-26 17:14:34 +0100

    Fix ormconfig loading Tyepscript entities

--- ormconfig.example.js
+++ ormconfig.example.js
@@ -3,9 +3,9 @@ const { join } = require("path");
 module.exports = {
   type: "sqlite",
   synchronize: true,
-  entities: [join(__dirname, "**", "*.entity.{ts,js}")],
+  entities: ["dist/**/*.entity.js"],
   database: "tmp/data.sqlite",
-  migrations: [join(__dirname, "database", "migrations", "**", "*.ts")],
+  migrations: [join(__dirname, "database", "migrations", "*.ts")],
   cli: {
     migrationsDir: "src/database/migrations",
   },
