46365abfc0d9df0bddb5bdc62da40a0c8e13e71c
Dan6erbond <moravrav@gmail.com>
2021-01-27 12:29:36 +0100

    Implement user login, me and register queries with custom auth service and GQL auth guards/user context

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "../auth/auth.module";
 import { User } from "./user.entity";
 import { UsersController } from "./users.controller";
 import { UsersResolver } from "./users.resolver";
 import { UsersService } from "./users.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   providers: [UsersResolver, UsersService],
   controllers: [UsersController],
   exports: [UsersService],
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,12 +1,22 @@
-import { Resolver, Query, Args, Mutation } from "@nestjs/graphql";
+import { UseGuards } from "@nestjs/common";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { UserInputError } from "apollo-server-express";
+import { AuthService } from "../auth/auth.service";
+import { CurrentUser } from "../auth/current-user.decorator";
+import { GqlAuthGuard } from "../auth/guard/gql-auth.guard";
+import { LoginUserDto } from "./dto/login-user.dto";
 import { UserDto } from "./dto/user.dto";
+import { LoginUserInput } from "./input/login-user.input";
+import { RegisterUserInput } from "./input/register-user.input";
+import { User } from "./user.entity";
 import { UsersService } from "./users.service";
-import { RegisterUserInput } from "./input/registerUser.input";
 
 @Resolver()
 export class UsersResolver {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Query(() => UserDto, { nullable: true })
   async user(
@@ -24,9 +34,39 @@ export class UsersResolver {
     }
   }
 
+  @Query(() => UserDto)
+  @UseGuards(GqlAuthGuard)
+  me(@CurrentUser() user: User) {
+    return this.usersService.findOne({ id: user.id });
+  }
+
+  @Mutation(() => LoginUserDto)
+  async login(@Args("input") loginInput: LoginUserInput) {
+    const user = await this.authService.validateUser(
+      loginInput.username,
+      loginInput.password,
+    );
+    if (!user) {
+      return new UserInputError("Username or password incorrect.");
+    }
+    const { accessToken } = await this.authService.login(user);
+    const payload = new LoginUserDto();
+    payload.accessToken = accessToken;
+    payload.user = new UserDto(user);
+    return payload;
+  }
+
   @Mutation(() => UserDto)
   async register(@Args("input") registerInput: RegisterUserInput) {
-    const user = await this.usersService.create(registerInput);
+    const user = await this.authService.register(
+      registerInput.username,
+      registerInput.password,
+    );
+    if (!user) {
+      return new UserInputError(
+        `User by username ${registerInput.username} already exists.`,
+      );
+    }
     return new UserDto(user);
   }
 }
