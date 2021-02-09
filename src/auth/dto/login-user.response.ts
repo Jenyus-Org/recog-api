27e499683f20d698b96a0e3f9765974b4ba7a7e6
Dan6erbond <moravrav@gmail.com>
2021-02-09 20:34:47 +0100

    Remove unnecessary exclude/expose decorators

--- src/auth/dto/login-user.response.ts
+++ src/auth/dto/login-user.response.ts
@@ -1,18 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Exclude, Expose } from "class-transformer";
 import { UserDto } from "../../users/dto/user.dto";
 
-@Exclude()
 export class LoginUserResponse {
-  @Expose()
   @ApiProperty()
   user: UserDto;
 
-  @Expose()
   @ApiProperty()
   accessToken: string;
 
-  @Expose()
   @ApiProperty()
   refreshToken: string;
 }
--- src/auth/dto/refresh-token.response.ts
+++ src/auth/dto/refresh-token.response.ts
@@ -1,14 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Exclude, Expose } from "class-transformer";
 import { UserDto } from "../../users/dto/user.dto";
 
-@Exclude()
 export class RefreshTokenResponse {
-  @Expose()
   @ApiProperty()
   user: UserDto;
 
-  @Expose()
   @ApiProperty()
   accessToken: string;
 }
