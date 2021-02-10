7b2c7b19172e7329d19a7f94f1b3f9679fd532fb
Dan6erbond <moravrav@gmail.com>
2021-02-10 14:42:44 +0100

    Update typing

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -1,9 +1,12 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { GqlExecutionContext } from "@nestjs/graphql";
+import { User } from "src/users/user.entity";
 
-export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req.user;
-  },
-);
+export const CurrentUser = createParamDecorator<
+  unknown,
+  ExecutionContext,
+  User
+>((data, context) => {
+  const ctx = GqlExecutionContext.create(context);
+  return ctx.getContext().req.user;
+});
