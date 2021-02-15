61e991e0497f491b5c5e79086dcb1f57d5686f70
Doemuu <doemu@outlook.com>
2021-02-15 21:49:31 +0100

    implement currentuser decorator for rest

---
+++ src/auth/gql-current-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+import { User } from "src/users/user.entity";
+
+export const GqlCurrentUser = createParamDecorator<
+  unknown,
+  ExecutionContext,
+  User
+>((data, context) => {
+  const ctx = GqlExecutionContext.create(context);
+  return ctx.getContext().req.user;
+});
