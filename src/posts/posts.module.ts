a35c25b19d7f319216e3c21b89f46f87938baba6
Dan6erbond <moravrav@gmail.com>
2021-02-22 23:10:58 +0100

    Fetch post flairs with flairs service

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,17 @@
 import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { FlairsModule } from "src/flairs/flairs.module";
 import { UsersModule } from "src/users/users.module";
 import { Post } from "./post.entity";
 import { PostsResolver } from "./posts.resolver";
 import { PostsService } from "./posts.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post]), forwardRef(() => UsersModule)],
+  imports: [
+    TypeOrmModule.forFeature([Post]),
+    forwardRef(() => UsersModule),
+    forwardRef(() => FlairsModule),
+  ],
   providers: [PostsResolver, PostsService],
   exports: [PostsService],
 })
--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,5 +1,6 @@
-import { Selections } from "@jenyus-org/nestjs-graphql-utils";
+import { HasFields, Selections } from "@jenyus-org/nestjs-graphql-utils";
 import { Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import { FlairsService } from "src/flairs/flairs.service";
 import { UsersService } from "src/users/users.service";
 import { PostObject } from "./dto/post.object";
 import { Post } from "./post.entity";
@@ -10,10 +11,17 @@ export class PostsResolver {
   constructor(
     private postsService: PostsService,
     private usersService: UsersService,
+    private flairsService: FlairsService,
   ) {}
 
   @Query(() => [PostObject])
-  async posts(@Selections("posts", ["author"]) relations: string[]) {
+  async posts(
+    @Selections("posts", ["author"]) relations: string[],
+    @HasFields("posts.flair") selectFlairs: boolean,
+  ) {
+    if (selectFlairs) {
+      relations = [...relations, "postToFlairs", "postToFlairs.flair"];
+    }
     return await this.postsService.findAll({ relations });
   }
 
@@ -23,5 +31,13 @@ export class PostsResolver {
       return post.author;
     }
     return await this.usersService.findOne({ postId: post.id });
+  }
+
+  @ResolveField()
+  async flairs(@Parent() post: Post) {
+    if (post.postToFlairs) {
+      return post.postToFlairs.map((postToFlair) => postToFlair.flair);
+    }
+    return await this.flairsService.findAll({ postId: post.id });
   }
 }
