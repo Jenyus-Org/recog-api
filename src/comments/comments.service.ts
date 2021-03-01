e77e6efeb2ff02bd7ba91586da4e5a6489eeaaa9
Dan6erbond <moravrav@gmail.com>
2021-03-01 22:09:20 +0100

    Add `getPost` to comments service for nested comments

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Post } from "src/posts/entities/post.entity";
 import { Repository } from "typeorm";
 import { Comment } from "./entities/comment.entity";
 
 interface FindAllArgs {
   postId?: number;
   authorId?: number;
+}
+
+interface FindOneArgs {
+  id: number;
+  relations?: string[];
 }
 
 @Injectable()
@@ -35,5 +41,31 @@ export class CommentsService {
     } else {
       throw new Error("One of post ID or author ID must be provided.");
     }
+  }
+
+  async findOne({ id, relations }: FindOneArgs) {
+    return await this.commentsRepository.findOne(id, { relations });
+  }
+
+  async getPost(comment: Comment) {
+    let currentComment: Comment = await this.commentsRepository.findOne(
+      comment.id,
+      {
+        relations: ["parentComment", "parentPost"],
+      },
+    );
+    let parentPost: Post = currentComment.parentPost;
+
+    while (!parentPost) {
+      currentComment = await this.commentsRepository.findOne(
+        currentComment.parentComment.id,
+        {
+          relations: ["parentComment", "parentPost"],
+        },
+      );
+      parentPost = currentComment.parentPost;
+    }
+
+    return parentPost;
   }
 }
